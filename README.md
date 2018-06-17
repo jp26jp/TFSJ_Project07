ae87e7d6b54ce62439d9cf6f9246fa866ee0ea28
John R Perry <john@j-26.com>
2018-06-16 18:20:05 -0600

    Completed project

---
+++ README.md
@@ -0,0 +1,1 @@
+To use this app, add your credentials to the config.js file located in the root of the folder!
---
+++ config.js
@@ -0,0 +1,8 @@
+const Twitter = require("twit")
+module.exports = new Twitter({
+                                 consumer_key       : "HIYGbW0hHm3p1p5Y4Gky28sO0",
+                                 consumer_secret    : "zcNXvhmoiuvjdBrKQjim35SGIaojDOA9ZRD9z4LyoXDN8BUkQx",
+                                 access_token       : "2222187433-MOCWIGDH6K82l8DelYHGwDrJByn9nMtHt70JiX7",
+                                 access_token_secret: "2ybJS7WVY0sH1U3ZTIwFi0QVBIiAeIVghusrEsT246E75"
+                             })
+
